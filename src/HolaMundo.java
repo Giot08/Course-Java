144a86aa2d5c421fdf3cdae0558652eff270f5f1
giot08 <xgiot08@gmail.com>
2022-12-28 00:47:38 -0300

    init

---
+++ src/HolaMundo.java
@@ -0,0 +1,9 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+
+        String name = "Jose Torres";
+        Integer edad = 28;
+        System.out.println(name);
+        System.out.println(edad);
+    }
+}
